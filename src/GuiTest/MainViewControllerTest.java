539249efeaf1972eba5166b31a899656b6207e0f
Caleb T. Vanorio <caleb.vanorio@centre.edu>
2018-04-23 17:14:42 -0400

    Use junit 5

--- src/GuiTest/MainViewControllerTest.java
+++ src/GuiTest/MainViewControllerTest.java
@@ -1,35 +1,60 @@
 package GuiTest;
 
-import org.junit.jupiter.api.Test;
-import org.testfx.framework.junit.ApplicationTest;
+import static org.junit.Assert.assertThat;
 
-import Client.Client;
+import java.io.IOException;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.testfx.api.FxRobot;
+import org.testfx.framework.junit5.Start;
+
+import Server.Server;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 import s4.Main;
 import s4.MainView.MainViewController;
+import org.testfx.framework.junit5.*;
+@ExtendWith(ApplicationExtension.class)
+public class MainViewControllerTest // extend ApplicationTest
 
-class MainViewControllerTest extends ApplicationTest
 {
-	Main main;
-	Client client;
+	BorderPane viewGoesHere = new BorderPane();
+	Stage stage;
 
-	@Override
-	public void start(Stage stage)
+	@Start
+	public void onStart(Stage stage) throws IOException
 	{
-		main.showLogin();
-		main.viewGoesHere.setId("#BorderPane");
-		Scene s = new Scene(main.viewGoesHere);
-		stage.setScene(s);
-		stage.show();
+		this.stage = stage;
+
+		FXMLLoader loader = new FXMLLoader();
+		loader.setLocation(Main.class.getResource("MainView/MainView.fxml"));
+		try
+		{
+			viewGoesHere.setCenter(loader.load());
+			MainViewController cont = loader.getController();
+			cont.setMain(new Main());
+			Server server = new Server();
+			cont.setServer(server);
+			Scene s = new Scene(viewGoesHere);
+			stage.setScene(s);
+			stage.show();
+
+		} catch (IOException e)
+		{
+			e.printStackTrace();
+		}
+		System.out.println("here");
 	}
 
 	@Test
-	void test()
+	public void please(FxRobot robo)
 	{
-		clickOn("#username");
-		write("Caleb");
-		
+		// assertThat();
+		//stage.getScene().getRoot();
+		robo.clickOn("#username");
 	}
 
 }
